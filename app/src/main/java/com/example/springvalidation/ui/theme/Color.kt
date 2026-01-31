6d333aeb86d69984579802a8253729289d6b775b
RomanchukSerhii <serhiiromanchuk@gmail.com>
2026-01-31 12:23:40 +0200

    feat(theme): apply custom spring color palette

--- app/src/main/java/com/example/springvalidation/ui/theme/Color.kt
+++ app/src/main/java/com/example/springvalidation/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.example.springvalidation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme colors
+val Primary = Color(0xFF7DA845)
+val Secondary = Color(0xFFDDE94F)
+val Tertiary = Color(0xFFFCF9D5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Background = Color(0xFFFFFFFF)
+val Surface = Color(0xFFF9FAEF)
+val SurfaceContainerHighest = Color(0xFFE2E3D8)
+
+val OnBackground = Color(0xFF1B1C18)
+val OnSurface = Color(0xFF1A1C16)
+val OnSurfaceVariant = Color(0xFF44483D)
+val OnPrimary = Color(0xFFFFFFFF)
+
+val SecondaryContainer = Color(0xFFDCE7C7)
+val OnSecondaryContainer = Color(0xFFDBE7C8)
+
+val Outline = Color(0xFF75796C)
